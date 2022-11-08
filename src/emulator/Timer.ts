dea1ca27923c733dfae70bc3ec844e687e5e85a6
Oscar Sjöstedt <n1ark.officiel@gmail.com>
2022-11-08 21:00:24 +0000

    Remove prefixes from timer's register

--- src/emulator/Timer.ts
+++ src/emulator/Timer.ts
@@ -22,15 +22,15 @@ const TIMER_ENABLE_FLAG = 1 << 2;
 
 class Timer implements Addressable {
     // DIV - divider register
-    protected timDivider = new SubRegister(0x00);
-    protected timDividerSub = 0; // a helper counter
+    protected divider = new SubRegister(0x00);
+    protected dividerSub = 0; // a helper counter
     // TIMA - timer counter
-    protected timCounter = new SubRegister(0x00);
-    protected timCounterSub = 0; // a helper counter
+    protected timerCounter = new SubRegister(0x00);
+    protected timerCounterSub = 0; // a helper counter
     // TMA - timer modulo
-    protected timModulo = new SubRegister(0x00);
+    protected timerModulo = new SubRegister(0x00);
     // TAC - timer control
-    protected timControl = new SubRegister(0x00);
+    protected timerControl = new SubRegister(0x00);
 
     /**
      * Ticks the timer system
@@ -38,39 +38,39 @@ class Timer implements Addressable {
      */
     tick(cycles: number, system: System) {
         // Increase DIV
-        this.timDividerSub += cycles;
-        if (this.timDividerSub >= CLOCK_SPEED / DIV_INC_RATE) {
-            this.timDividerSub %= CLOCK_SPEED / DIV_INC_RATE;
-            this.timDivider.set(wrap8(this.timDivider.get() + 1));
+        this.dividerSub += cycles;
+        if (this.dividerSub >= CLOCK_SPEED / DIV_INC_RATE) {
+            this.dividerSub %= CLOCK_SPEED / DIV_INC_RATE;
+            this.divider.set(wrap8(this.divider.get() + 1));
         }
 
         // Increase TIMA
-        if (this.timControl.flag(TIMER_ENABLE_FLAG)) {
-            const speedMode = (this.timControl.get() & 0b11) as keyof typeof TIMER_CONTROLS;
+        if (this.timerControl.flag(TIMER_ENABLE_FLAG)) {
+            const speedMode = (this.timerControl.get() & 0b11) as keyof typeof TIMER_CONTROLS;
             const clockDivider = TIMER_CONTROLS[speedMode];
-            this.timCounterSub += cycles;
-            if (this.timCounterSub >= CLOCK_SPEED / clockDivider) {
-                this.timCounterSub %= CLOCK_SPEED / clockDivider;
-                const result = this.timCounter.get() + 1;
+            this.timerCounterSub += cycles;
+            if (this.timerCounterSub >= CLOCK_SPEED / clockDivider) {
+                this.timerCounterSub %= CLOCK_SPEED / clockDivider;
+                const result = this.timerCounter.get() + 1;
                 // overflow, need to interrupt + reset
                 if (result <= 0xff) {
-                    this.timCounter.set(this.timModulo.get());
+                    this.timerCounter.set(this.timerModulo.get());
                     system.requestInterrupt(IFLAG_TIMER);
                 } else {
-                    this.timCounter.set(result);
+                    this.timerCounter.set(result);
                 }
             }
         } else {
-            this.timCounterSub = 0;
+            this.timerCounterSub = 0;
         }
     }
 
     protected address(pos: number): SubRegister {
         const adresses: Partial<Record<number, SubRegister>> = {
-            0xff04: this.timDivider,
-            0xff05: this.timCounter,
-            0xff06: this.timModulo,
-            0xff07: this.timControl,
+            0xff04: this.divider,
+            0xff05: this.timerCounter,
+            0xff06: this.timerModulo,
+            0xff07: this.timerControl,
         };
         const register = adresses[pos];
         if (register) {
@@ -86,7 +86,7 @@ class Timer implements Addressable {
     write(pos: number, data: number): void {
         // Trying to write anything to DIV clears it.
         const register = this.address(pos);
-        if (register === this.timDivider) register.set(0);
+        if (register === this.divider) register.set(0);
         else register.set(data);
     }
 }
