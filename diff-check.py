8eeb1726c358dae879f863ae2ee6c345b35131e3
Oscar Sjöstedt <n1ark.officiel@gmail.com>
2022-12-03 21:48:55 +0000

    Add parameters to diff-check script

--- diff-check.py
+++ diff-check.py
@@ -1,9 +1,9 @@
+import sys
 from termcolor import colored
 
 
 def parse(l: str):
-    parts = l.split("/")
-    return {p[0]: p[1] for r in parts if len(p := r.split(":")) > 1}
+    return {p[0]: p[1] for r in l.split("/") if len(p := r.split(":")) > 1}
 
 
 def ranges(vals: list[int]) -> list[tuple[int, int]]:
@@ -21,8 +21,17 @@ def ranges(vals: list[int]) -> list[tuple[int, int]]:
     return ranges
 
 
-def main():
+def diff_lines(line1: str, line2: str):
+    pl1 = parse(line1)
+    pl2 = parse(line2)
+    return {
+        k: {1: pl1[k], 2: pl2[k] if k in pl2.keys() else "(not set)"}
+        for k in pl1.keys()
+        if (k not in pl2.keys() or pl1[k] != pl2[k])
+    }
 
+
+def main(ignored: list[str] = [], limit: int = 64):
     with open("log_em1.txt", "r") as log1_file:
         log1 = log1_file.readlines()
 
@@ -44,17 +53,22 @@ def main():
         diff = {
             k: {1: pl1[k], 2: pl2[k] if k in pl2.keys() else "(not set)"}
             for k in pl1.keys()
-            if k not in pl2.keys() or pl1[k] != pl2[k]
+            if (k not in pl2.keys() or pl1[k] != pl2[k]) and k not in ignored
         }
+        if not diff:
+            continue
         print(colored(f"diff on line {i}: {diff}", "red", attrs=["bold"]))
         for j in range(3):
             if i - (3 - j) < 0:
                 continue
+            prev_diff = diff_lines(log1[i-(3-j)], log2[i-(3-j)])
             print(f"Previous {3-j}: {log1[i-(3-j)]}", end="")
+            if(prev_diff):
+                print(f"\tdiff was: {prev_diff}")
         print(colored(f"Emulator 1: {l1}", "red"), end="")
         print(colored(f"Emulator 2: {l2}", "green"))
         mistakes += [i]
-        if len(mistakes) > 64:
+        if len(mistakes) > limit:
             print("ending because too many mistakes")
             break
 
@@ -69,6 +83,16 @@ def main():
             ),
         )
 
+def find_arg(name: str):
+    for a in sys.argv:
+        if a.startswith(f"--{name}="):
+            return a.replace(f"--{name}=", "")
+    return None
 
 if __name__ == "__main__":
-    main()
+    kwargs = {}
+    if ignored := find_arg("i"):
+        kwargs["ignored"] = ignored.split(",")
+    if limit := find_arg("l"):
+        kwargs["limit"] = int(limit)
+    main(**kwargs)
