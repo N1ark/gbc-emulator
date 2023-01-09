2f03918923c191029fa927341983e33b010d3d28
Oscar Sjöstedt <n1ark.officiel@gmail.com>
2023-01-09 23:53:13 +0000

    Change `<GraphicsDrawer/>` icons, add local storage saving

--- src/Drawer/GraphicsDrawer.tsx
+++ src/Drawer/GraphicsDrawer.tsx
@@ -1,5 +1,7 @@
-import { Circle, CircleDot, Target } from "lucide-preact";
+import { Circle, CircleDot, Grid, Square, Target } from "lucide-preact";
 import { FunctionalComponent } from "preact";
+import { useEffect } from "preact/hooks";
+import Grid2x from "../Grid2x";
 import { Identity, ImageFilter, Scale2x, Scale4x } from "../ImageFilter";
 
 type GraphicsDrawerProps = {
@@ -7,36 +9,66 @@ type GraphicsDrawerProps = {
     setFilter: (f: ImageFilter) => void;
 };
 
+const localStorageKey = "graphics-drawer-filter";
+
 const GraphicsDrawer: FunctionalComponent<GraphicsDrawerProps> = ({
     currentFilter,
     setFilter,
-}) => (
-    <div>
-        <div className="drawer-section-title">
-            <div>Filter:</div>
-            <button
-                title="identity"
-                className={`icon-button ${currentFilter === Identity ? "toggled" : ""}`}
-                onClick={() => setFilter(Identity)}
-            >
-                <Circle />
-            </button>
-            <button
-                title="scale2x"
-                className={`icon-button ${currentFilter === Scale2x ? "toggled" : ""}`}
-                onClick={() => setFilter(Scale2x)}
-            >
-                <CircleDot />
-            </button>
-            <button
-                title="scale4x"
-                className={`icon-button ${currentFilter === Scale4x ? "toggled" : ""}`}
-                onClick={() => setFilter(Scale4x)}
-            >
-                <Target />
-            </button>
+}) => {
+    useEffect(() => {
+        const savedFilter = localStorage.getItem(localStorageKey);
+        if (savedFilter) {
+            setFilter(
+                {
+                    identity: Identity,
+                    scale2x: Scale2x,
+                    scale4x: Scale4x,
+                }[savedFilter] ?? Identity
+            );
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(
+            localStorageKey,
+            currentFilter === Identity
+                ? "identity"
+                : currentFilter === Scale4x
+                ? "scale4x"
+                : currentFilter === Scale2x
+                ? "scale2x"
+                : ""
+        );
+    }, [currentFilter]);
+
+    return (
+        <div>
+            <div className="drawer-section-title">
+                <div>Filter:</div>
+                <button
+                    title="identity"
+                    className={`icon-button ${currentFilter === Identity ? "toggled" : ""}`}
+                    onClick={() => setFilter(Identity)}
+                >
+                    <Square />
+                </button>
+                <button
+                    title="scale2x"
+                    className={`icon-button ${currentFilter === Scale2x ? "toggled" : ""}`}
+                    onClick={() => setFilter(Scale2x)}
+                >
+                    <Grid2x />
+                </button>
+                <button
+                    title="scale4x"
+                    className={`icon-button ${currentFilter === Scale4x ? "toggled" : ""}`}
+                    onClick={() => setFilter(Scale4x)}
+                >
+                    <Grid />
+                </button>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default GraphicsDrawer;
---
+++ src/Grid2x.ts
@@ -0,0 +1,93 @@
+import { ComponentType, h, toChildArray } from "preact";
+import { JSX } from "preact/jsx-runtime";
+
+const defaultAttributes = {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: 24,
+    height: 24,
+    viewBox: "0 0 24 24",
+    fill: "none",
+    stroke: "currentColor",
+    "stroke-width": 2,
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+};
+
+type IconNode = [elementName: keyof JSX.IntrinsicElements, attrs: Record<string, string>][];
+
+interface LucideProps extends Omit<JSX.SVGAttributes<SVGElement>, "ref" | "size"> {
+    size?: string | number;
+    color?: string;
+}
+
+export const toKebabCase = (string: string) =>
+    string.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
+
+/**
+ * Taken from Preact library directly:
+ * @link https://github.com/lucide-icons/lucide/blob/main/packages/lucide-preact/src/createPreactComponent.ts
+ */
+const createPreactComponent = (iconName: string, iconNode: IconNode) => {
+    const Component = ({
+        color = "currentColor",
+        size = 24,
+        strokeWidth = 2,
+        children,
+        ...rest
+    }: LucideProps) =>
+        h(
+            "svg" as unknown as ComponentType<
+                Partial<JSX.SVGAttributes<SVGElement> & { "stroke-width": number | string }>
+            >,
+            {
+                ...defaultAttributes,
+                width: String(size),
+                height: size,
+                stroke: color,
+                ["stroke-width" as "strokeWidth"]: strokeWidth,
+                class: `lucide lucide-${toKebabCase(iconName)}`,
+                ...rest,
+            },
+            [...iconNode.map(([tag, attrs]) => h(tag, attrs)), ...toChildArray(children)]
+        );
+
+    Component.displayName = `${iconName}`;
+
+    return Component;
+};
+const Grid2x = createPreactComponent("Grid2x", [
+    [
+        "rect",
+        {
+            x: "3",
+            y: "3",
+            width: "18",
+            height: "18",
+            rx: "2",
+            ry: "2",
+            key: "maln0c",
+        },
+    ],
+    [
+        "line",
+        {
+            x1: "3",
+            y1: "12",
+            x2: "21",
+            y2: "12",
+            key: "1uch6j",
+        },
+    ],
+    [
+        "line",
+        {
+            x1: "12",
+            y1: "3",
+            x2: "12",
+            y2: "21",
+            key: "nvcl17",
+        },
+    ],
+]);
+
+export default Grid2x;
