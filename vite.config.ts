ecd5ad81e9013ecc3811c0e552b473265f14cf7e
N1ark <opale.sjostedt@gmail.com>
2025-11-16 15:05:50 +0000

    Update versions + Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import preact from "@preact/preset-vite";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [preact()],
+    plugins: [preact(), cloudflare()],
     resolve: {
         alias: {
             "@frontend": path.resolve(__dirname, "src", "frontend"),
